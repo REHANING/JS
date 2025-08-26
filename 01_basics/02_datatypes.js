05f00a794b78653c5b1a23a4d4b377eec2e2698a
REHAN AHMED <123252096+REHANING@users.noreply.github.com>
2025-08-26 02:56:42 +0000

    datatypes in JS

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict" //treat all JS code as newer version
+
+//alert(3+3) // we are using nodeJS , not browser
+
+/*console.log(3 
+    + 3) // code readability should be high
+console.log("rehan")
+*/
+let name = "rehan"
+let age = 22
+let isloggedin = false
+let state; //undefined
+
+//number => integers
+//sting => ""
+//bigint => for big integers
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+console.log(typeof "rehan")
+console.log(typeof "state")
