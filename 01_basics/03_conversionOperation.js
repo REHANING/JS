1764f0f1d6aef4e986a606605b0db906bf3dc100
REHAN AHMED <123252096+REHANING@users.noreply.github.com>
2025-08-26 03:31:53 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -24,4 +24,31 @@ let isLoggedIn = 1
 
 let someNumber = 33
 let isString = String (someNumber)
-console.log(typeof isString);
+// console.log(typeof isString);
+
+// ********************Operations*************************
+
+let value = 3
+let negValue = -value
+//console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "rehan"
+let str2 = "ahmed"
+
+let str3 = str1+str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+
+
+
