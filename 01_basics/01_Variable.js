6567f38fe4a0f9b187a51fde02d27d26aaa0f6fb
REHAN AHMED <123252096+REHANING@users.noreply.github.com>
2025-08-26 02:37:13 +0000

    variables in js

---
+++ 01_basics/01_Variable.js
@@ -0,0 +1,16 @@
+const accountId = "12345"
+let accountEmail = "rehan@google.com"
+var accountPassword = "3213"
+accountCity = "lhr" //not a good practice
+let accountState;
+
+//not allowed while using const
+//accountId = "1212"
+
+accountEmail = "ahmed@google.com"
+accountPassword = "3232"
+accountCity = "Khi"
+
+console.log(accountId);
+
+console.log([accountId, accountEmail, accountPassword, accountCity, accountState])
