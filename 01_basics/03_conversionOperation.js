687357f090010bf40722079651399a2b580a26e3
REHAN AHMED <123252096+REHANING@users.noreply.github.com>
2025-08-26 03:19:59 +0000

    conversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "33abc"
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let valueInNumber = Number (score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => Nan
+// true = 1 or false = 0
+
+let isLoggedIn = 1
+
+// let booleanIsLoggedIn = Boolean (isLoggedIn)
+// console.log(typeof booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true
+// 0 => false
+// "" => false
+// "rehan" => true
+
+let someNumber = 33
+let isString = String (someNumber)
+console.log(typeof isString);
